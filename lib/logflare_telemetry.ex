2579ca621457ea3764f753fe784c81617d77e1a8
ontofractal <v@ontofractal.com>
2020-10-08 17:38:43 +0300

    init logflare telemetry 📈

---
+++ lib/logflare_telemetry.ex
@@ -0,0 +1,18 @@
+defmodule LogflareTelemetry do
+  @moduledoc """
+  Documentation for `LogflareTelemetry`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LogflareTelemetry.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/logflare_telemetry_test.exs
@@ -0,0 +1,8 @@
+defmodule LogflareTelemetryTest do
+  use ExUnit.Case
+  doctest LogflareTelemetry
+
+  test "greets the world" do
+    assert LogflareTelemetry.hello() == :world
+  end
+end
